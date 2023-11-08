e5a714c052ec491d64827bbd0ad8010fbccfe0d5
notNathanlol <nathandurrant726@gmail.com>
2023-11-07 21:04:04 -0800

    Create user functionality

--- src/Library.java
+++ src/Library.java
@@ -9,4 +9,61 @@ public class Library {
         PremiumUser payToWin = (PremiumUser) u;
         u = payToWin;
     }
+
+    // method to add a user to the system
+    // called after a user enters their information in signup page
+    public static void addUser(String username, char[] password) throws PasswordException{
+        //Throw exception if password is less than 8 characters
+        if (password.length < 8) {
+            throw new Minimum8CharactersRequired();
+        }
+        //Create flag for exception
+        boolean flag = true;
+        //Check if password has an upper case char
+        for (int i = 0; i < password.length; i++) {
+            if (Character.isUpperCase(password[i])) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            PasswordException UpperCaseCharacterMissing = new UpperCaseCharacterMissing();
+            throw UpperCaseCharacterMissing;
+        }
+        flag = true;
+        //check for lower case character
+        for (int i = 0; i < password.length; i++) {
+            if (Character.isLowerCase(password[i])) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            PasswordException LowerCaseCharacterMissing = new LowerCaseCharacterMissing();
+            throw LowerCaseCharacterMissing;
+        }
+        flag = true;
+        //check if there is a special character
+        for (int i = 0; i < password.length; i++) {
+            if (!Character.isDigit(password[i]) && !Character.isLetter(password[i])
+                    && !Character.isWhitespace(password[i])) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            PasswordException SpecialCharacterMissing = new SpecialCharacterMissing();
+            throw SpecialCharacterMissing;
+        }
+        flag = true;
+        //check for a number character
+        for (int i = 0; i < password.length; i++) {
+            if (Character.isDigit(password[i])) {
+                flag = false;
+            }
+        }
+        if (flag) {
+            PasswordException NumberCharacterMissing = new NumberCharacterMissing();
+            throw NumberCharacterMissing;
+        }
+        User u = new User(username, password.toString());
+        users.add(u);
+    }
 }
