0f4387950fb9c46877a7922214cdcf717698fa1b
huuducvo <huuduc.vo@sjsu.edu>
2023-11-09 19:57:27 -0800

    HUU

--- src/Librarian.java
+++ src/Librarian.java
@@ -1,29 +1,43 @@
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.Objects;
 
 public class Librarian extends User{
 
+    public Librarian(){}
+
     public Librarian(String username, String password, String security1, String security2){
         super(username, password, security1, security2);
     }
 
-    public void addBook(Book book){
-        for(Book b : getBooks()){
-            if(b != book){
-                getBooks().add(book);
-            } else {
-                System.out.println("Book already in the library system"); // GUI message
+    // Librarian enter book information to add book to library system
+    public void addBook(String name, String author, LinkedList<Book.genre> genreList, int year, Date dueDate){
+        int count = 0;
+        for(Book book : ((Library)this).getBooks()){
+            // Check using only name, author, and year
+            if(Objects.equals(book.getName(), name) && Objects.equals(book.getAuthor(), author) && book.getYear() == year){
+                System.out.println("Book already in library");
+                count++;
             }
+        }
+        if(count == 0) {
+            Book book = new Book(name, author, genreList, year, dueDate);
+            ((Library)this).getBooks().add(book);
         }
     }
 
-    public void removeBook(Book book){
-        if(getBooks().contains(book)) {
-            getBooks().remove(book);
-            System.out.println("Book has been remove from the library system"); // GUI message
-        } else {
-            System.out.println("Book is not in the library system"); // GUI message
+    // Librarian type in the name of the book they want to remove
+    public void removeBook(String bookName){
+        for(Book book : ((Library)this).getBooks()){
+            if(Objects.equals(book.getName(), bookName)){
+                ((Library)this).getBooks().remove(book);
+                System.out.println("Book has been removed");  //GUI message
+            } else {
+                System.out.println("Book is not in library system, please try again");
+                // GUI message
+            }
         }
     }
 
@@ -36,8 +50,13 @@ public class Librarian extends User{
         }
     }
 
-    public ArrayList<Book> checkUserCheckedOut(User user){
-        return user.getBooks();
+    public ArrayList<Book> checkUserCheckedOut(String username){
+        if(users.containsKey(username)){
+            return users.get(username).getBooks();
+        } else {
+            System.out.println("User not existed, please try again"); // GUI message
+        }
+        return null;
     }
 
     public ArrayList<Book> checkSuggestions(){
@@ -45,10 +64,10 @@ public class Librarian extends User{
     }
 
     // Librarian type in book name and a list of genre they would like to add to that book
-    public void addBookGenre(String name, LinkedList<Book.genre> genrelist){
+    public void addBookGenre(String name, LinkedList<Book.genre> genreList){
         for(int i = 0; i < getBooks().size(); i++){
             if(Objects.equals(getBooks().get(i).getName(), name)){
-                getBooks().get(i).getGenreList().addAll(genrelist);
+                getBooks().get(i).getGenreList().addAll(genreList);
             }
         }
     }
